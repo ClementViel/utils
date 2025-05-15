ed756a30e410d9028990028395ea27ac2d7d5121
Clement Viel <cviel@missingno.tech>
2025-05-15 17:20:38 +0200

    lookup function: return all struct from Vec<Struct> that satisfies the condition struct.field 1 == field
    still not compiling though

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,22 +1,17 @@
-use std::iter::FromIterator;
-
-#[derive(FromIterator)]
 struct Generic<T> {
     field1: T,
     field2: T,
 }
 
-fn lookup<T: PartialEq>(
-    orig_struct: Vec<Generic<T>>,
-    target_stuct: Vec<Generic<T>>,
-    field: Option<T>,
-) {
-    let result: Vec<Generic<T>> = if let Some(target_field) = field {
+fn lookup<T: PartialEq>(orig_struct: Vec<Generic<T>>, field: Option<T>) -> Vec<&Generic<T>> {
+    let result: Vec<&Generic<T>> = if let Some(target_field) = field {
         orig_struct
             .iter()
             .filter(|o| o.field1 == target_field)
             .collect()
     } else {
-        orig_struct.collect();
+        orig_struct.iter().collect()
     };
+
+    result
 }
